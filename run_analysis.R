95ae978097e693791d9b924de2b6d2577b50912f
Rodrigo Sant'Ana <santana@chewie.home>
2015-05-24 13:03:19 -0300

    First commit - Creating R script

---
+++ run_analysis.R
@@ -0,0 +1,51 @@
+########################################################################
+## Description: Getting and Cleaning Data Course Project
+## 
+## Maintainer: Johns Hopkins University/Coursera
+## Author: Rodrigo Sant'Ana
+## Created: Dom Mai 24 12:41:19 2015 (-0300)
+## Version: 0.0.1
+## Last-Updated: Dom Mai 24 13:01:24 2015 (-0300)
+##           By: Rodrigo Sant'Ana
+## 
+## URL: github.com/rodrigosantana
+## Doc URL: github.com/rodrigosantana
+## 
+## Database info:
+## http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+## 
+### Commentary: This code is part of Course Project in Getting and
+### Cleaning Data - Johns Hopkins University/Coursera
+## 
+### Code:
+########################################################################
+
+###
+
+########################################################################
+## 
+## The MIT License (MIT)
+## 
+## Copyright (c) 2014 Rodrigo Sant'Ana
+## 
+## Permission is hereby granted, free of charge, to any person obtaining a
+## copy of this software and associated documentation files (the
+## 'Software'), to deal in the Software without restriction, including
+## without limitation the rights to use, copy, modify, merge, publish,
+## distribute, sublicense, and/or sell copies of the Software, and to
+## permit persons to whom the Software is furnished to do so, subject to
+## the following conditions:
+## 
+## The above copyright notice and this permission notice shall be
+## included in all copies or substantial portions of the Software.
+## 
+## THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+## EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+## OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+## NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+## BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+## ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+## CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+## SOFTWARE.
+## 
+########################################################################
